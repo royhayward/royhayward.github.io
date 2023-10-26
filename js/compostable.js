1bc4d8596f459ddf1b9f4d790233e029b41d385b
Roy Hayward <roy@ponderize.me>
2023-10-26 15:46:39 -0600

    Restructure and refine CSS and JS

--- js/compostable.js
+++ js/compostable.js
@@ -1,34 +1,60 @@
+function getRandomCard(e) {
+  fetch("cards/")
+    .then((response) => response.text())
+    .then((data) => {
+      const files = data.split("\\n");
+      const randomIndex = Math.floor(Math.random() * files.length);
+      const randomFile = files[randomIndex].trim();
+      console.log(randomFile);
+      fetch(`cards/${randomFile}`)
+        .then((response) => response.text())
+        .then((cardData) => {
+          e.innerHTML = cardData;
+        })
+        .catch((error) => console.log(error));
+    })
+    .catch((error) => console.log(error));
+}
+function getPageName() {
+  const url = window.location.href;
+  const lastSlashIndex = url.lastIndexOf("/");
+  const pageWithExtension = url.substring(lastSlashIndex + 1);
+  const pageName = pageWithExtension.split(".")[0];
+  return pageName;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-    const elements = document.querySelectorAll("body > [id]");
-    elements.forEach(element => {
-      const pageName = element.id;
-      const pageUrl = `comp/${pageName}.html`;
-      fetch(pageUrl)
-        .then(response => {
-          if (response.ok) {
-            return response.text();
-          } else {
-            throw new Error("File not found");
-          }
-        })
-        .then(data => {
-          element.innerHTML = data;
-        })
-        .catch(error => console.log(error));
-    });
+  const elements = document.querySelectorAll("body > [id]");
+  elements.forEach((element) => {
+    var pageName = element.id;
+    if (pageName == "index"){
+        var dir = "pages";
+        var pageName = 'index'; 
+    } else if (pageName == "article") {
+      var dir = "pages";
+      var pageName = getPageName();
+    } else {
+      var dir = "comp";
+    }
+    const pageUrl = `${dir}/${pageName}.html`;
+    fetch(pageUrl)
+      .then((response) => {
+        if (response.ok) {
+          return response.text();
+        } else {
+          throw new Error("File not found");
+        }
+      })
+      .then((data) => {
+        element.innerHTML = data;
+      })
+      .catch((error) => console.log(error));
+  });
 
-    const navbarItems = document.querySelectorAll(".navbar li");
+  const navbarItems = document.querySelectorAll(".navbar li");
 
-    navbarItems.forEach(item => {
-        const childUl = item.querySelector("ul");
-
-        item.addEventListener("mouseenter", () => {
-            childUl.style.display = "block";
-        });
-
-        item.addEventListener("mouseleave", () => {
-            childUl.style.display = "none";
-        });
-    });
+  const pickACardElements = document.querySelectorAll("#pick-a-card");
+  pickACardElements.forEach((element) => {
+    getRandomCard(element);
+  });
 });
-
